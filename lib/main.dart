ee9253db8d5702ce455eb0541f648e344089c20c
Naruemarn <Naruemarn@hotmail.com>
2024-03-05 21:44:55 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter 55 Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter 123 Demo Home Page'),
     );
   }
 }
